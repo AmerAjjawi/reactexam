2209cdd6eccc12ec67701f095e555ea7f3c3849a
ajjawi_amer@hotmail.com <ajjawi_amer@hotmail.com>
2023-10-19 01:23:06 -0400

    Creating the validation rules on the form component with the states.

--- src/Forms.js
+++ src/Forms.js
@@ -1,2 +1,33 @@
 import React, { Component } from 'react';
-import { FormErros } from './Form'
+import { FormErrors } from './FormErrors';
+import './index.css';
+
+class Form extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            name: 'Please, enter your name',
+            email: 'Please, enter your email',
+            password: 'Please, enter your password',
+            FormErrors: {email: '', password: ''},
+            isEmailValid: false,
+            isPasswordValid: false,
+            formValid: false,
+            description: 'Please, enter the product',
+            category: 'Please, enter your category',
+            quantity: 'Please, enter the quantity you want'
+
+        }
+    }
+
+    inputValidation = (e) => {
+        const name = e.target.name;
+        const value = e.target.value; // Change "name" to "value"
+        this.setState(
+          { [name]: value },
+          () => {
+            this.fieldValidation(name, value);
+          }
+        );
+      }
+    
